eb255cbec58ef88e8c4ab0e66424c7f4c10c3919
Emilio Canchola <emilio.canchola@sdggroup.com>
2026-02-06 10:47:06 +0000

    syntax update

--- models/business/dim_supplier.sql
+++ models/business/dim_supplier.sql
@@ -17,7 +17,7 @@ regions AS (
 final AS (
     SELECT
         -- Use your macro to create a unique surrogate key
-        {{ gen_surkey(['s.supplier_key']) }} AS dim_supplier_key,
+        {{ gen_surkey(['supplier_key']) }} AS dim_supplier_key,
         s.supplier_key,
         s.supplier_name,
         s.address,
