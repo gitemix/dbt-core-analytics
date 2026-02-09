5611f2baef35ef4db7b7a6709af5947ffb50186d
Emilio Canchola <emilio.canchola@sdggroup.com>
2026-02-09 09:14:29 +0000

    test update

--- tests/date_integrity.sql
+++ tests/date_integrity.sql
@@ -5,5 +5,5 @@ SELECT
     l_commitdate,
     l_receiptdate
 FROM {{ ref('stg_tpch_lineitem') }}
-WHERE l_shipdate > l_receiptdate 
-   OR l_commitdate > l_receiptdate
+WHERE l_shipdate < l_receiptdate 
+   OR l_commitdate < l_receiptdate
